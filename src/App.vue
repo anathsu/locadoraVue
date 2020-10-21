6bdc2848df3aea518737ed5c50fe3768c5ef11a2
anathsu <willian.silva@al.infnet.edu.br>
2020-10-21 12:00:37 -0300

    adicionado cards

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,31 @@
       <p>{{dia()}}</p>
     </b-row>
 
+    <b-row>
+      <div class="cards">
+        <b-card v-bind:key="filme.id" v-for="filme in filmes"
+          :title="filme.titulo | maiuscula()"
+          :img-src="filme.imagem"
+          :img-alt="filme.descricao"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2 unitCard"
+        >
+          <b-card-text>
+            {{filme.descricao | maiuscula()}}
+          </b-card-text>
+
+          <b-card-text>
+            {{filme.valor | formatarPreco("R$")}}
+          </b-card-text>
+
+          <b-button class="btn" href="#" block variant="primary">Alugar</b-button>
+        </b-card>
+
+      </div>
+    </b-row>
+
   </b-container>
 
 
@@ -40,9 +65,18 @@ export default {
   }, data(){
     return {
       title: "Locado de Filmes",
-      horas: new Date().getHours()
+      horas: new Date().getHours(),
+      filmes: [
+        { id: 1, titulo: "Homem de Ferro", descricao: "Onde nasce o herói", valor: 25, imagem: "https://i.imgur.com/OA8pDFM.jpeg" },
+        { id: 2, titulo: "Pantera Negra", descricao: "Um filme de panteras", valor: 35, imagem: "https://i.imgur.com/JOSEGKf.jpeg" },
+        { id: 3, titulo: "Homem-Formiga", descricao: "Um filme de formigas", valor: 20, imagem: "https://i.imgur.com/zdi4zcy.jpeg" },
+        { id: 4, titulo: "Capitã Marvel", descricao: "Um filme de capitãs", valor: 40, imagem: "https://i.imgur.com/39aIfzI.jpeg" },
+        { id: 5, titulo: "Hulk", descricao: "Um filme de força", valor: 10, imagem: "https://i.imgur.com/0uthCmp.jpeg" },
+        { id: 6, titulo: "Homem Aranha - Longe de Casa", descricao: "Um filme de aranhas voadoras", valor: 11, imagem: "https://i.imgur.com/H1cRygg.jpeg" }
+      ]
     }
-  }, methods:{
+  },
+  methods:{
     dia(){
       if(this.horas < 12){
         return "Está de dia";
@@ -52,6 +86,18 @@ export default {
         return "Está de noite";
       }
     }
+  },
+  filters: {
+    formatarPreco: function(preco, simbolo) {
+      if(!parseInt(preco)) {
+        return "";
+      }
+      var precoFormatado = (preco.toFixed(2)).replace('.', ',');
+      return simbolo + " " + precoFormatado;
+    },
+    maiuscula: function(string){
+      return string.toUpperCase();
+    }
   }
 }
 </script>
@@ -60,6 +106,22 @@ export default {
 
 .row{
   justify-content: center;
+}
+
+.cards{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  text-align: center;
+  /* padding: 5px; */
+}
+
+.unitCard:hover{
+  border: rgb(7, 235, 56) 5px solid;
+}
+.btn:hover{
+  background-color: blueviolet;
+
 }
 
 

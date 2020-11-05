2abcccfa2afc4d7050af3aeda1c4cdb2e1064d43
anathsu <willian.silva@al.infnet.edu.br>
2020-11-05 11:43:25 -0300

    Componentes

---
+++ src/components/ResumoPedido.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-row>
+        <b-row>
+            <h2>Resumo do Carrinho</h2>
+            <b-table block fixed striped hover small head-variant="dark" :items="carrinho" :fields="campos"></b-table>
+        </b-row>
+        <b-row><p>Soma total do pedido: R$ {{somaPedido}},00</p></b-row>
+    </b-row>
+</template>
+
+<script>
+export default {
+  name: 'ResumoPedido',
+  props: {
+    carrinho: Array,
+  },
+  data(){
+      return{
+          campos: ['titulo', 'preço', 'quantidade'],
+      };
+  },
+  computed:{
+      somaPedido: function(){
+      return this.carrinho.reduce(function (accumulator, filme){
+        return accumulator + (filme.valor * filme.quantidade);
+      }, 0)
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
